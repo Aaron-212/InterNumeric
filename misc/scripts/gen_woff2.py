14eedbb93d7bc28593715eb4a38f8771394a5e41
Aaron Ruan <aaron212cn@outlook.com>
2024-03-31 17:38:25 +0800

    1.001update3

--- misc/scripts/gen_woff2.py
+++ misc/scripts/gen_woff2.py
@@ -1,8 +1,8 @@
 from fontTools.ttLib.woff2 import compress
 
 # Usage example
-otf_path = 'fonts/variable/InterNumeric[wght,rdns].otf'
-woff2_path = 'fonts/variable/InterNumeric[wght,rdns].woff2'
+otf_path = 'fonts/variable/InterNumeric[wght,RDNS].otf'
+woff2_path = 'fonts/variable/InterNumeric[wght,RDNS].woff2'
 
 # Convert the variable font to WOFF2 format
 compress(otf_path, woff2_path)
