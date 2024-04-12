18737afcc36518a2e8f558bc49c134d25f6ff46f
Aaron Ruan <aaron212cn@outlook.com>
2024-04-12 20:32:30 +0800

    STAT update

--- Makefile
+++ Makefile
@@ -13,15 +13,15 @@ init: requirements.txt
 
 build: build.stamp
 
-# fontmake -m "fonts-temp/master-ufo/InterNumeric.designspace" -o variable --output-path "fonts/variable/InterNumeric[wght,RDNS].ttf" --filter DecomposeTransformedComponentsFilter
+# fontmake -m "fonts-temp/master-ufo/InterNumeric.designspace" -o variable --output-path "fonts/variable/InterNumeric[wght,RDNS].ttf" --filter DecomposeTransformedComponentsFilter --verbose DEBUG
 # fontmake -m "fonts-temp/master-ufo/InterNumeric.designspace" -o variable-cff2 --output-path "fonts/variable/InterNumeric[wght,RDNS].otf"
 
 
 build.stamp: init.stamp
 	fontmake -g "src/InterNumeric.glyphspackage" -o ufo --output-dir "fonts-temp/master-ufo" --filter DecomposeTransformedComponentsFilter
 	python misc/scripts/copy_kern.py
-	fontmake -m "fonts-temp/master-ufo/InterNumeric.designspace" -o variable --output-path "fonts/variable/InterNumeric[wght,RDNS].ttf" --filter DecomposeTransformedComponentsFilter
 	python misc/scripts/stat.py
+	fontmake -m "fonts-temp/master-ufo/InterNumeric.designspace" -o variable --output-path "fonts/variable/InterNumeric[wght,RDNS].ttf" --filter DecomposeTransformedComponentsFilter
 	python misc/scripts/gen_woff2.py
 	touch build.stamp
 
--- misc/scripts/stat.py
+++ misc/scripts/stat.py
@@ -1,36 +1,61 @@
-from fontTools.otlLib.builder import buildStatTable
-from fontTools.ttLib.ttFont import TTFont
+from fontTools.designspaceLib import DesignSpaceDocument, LocationLabelDescriptor, AxisLabelDescriptor
 
-path = "fonts/variable/InterNumeric[wght,RDNS].ttf"
-font = TTFont(path)
+path = "fonts-temp/master-ufo/InterNumeric.designspace"
+dsfile = DesignSpaceDocument.fromfile(path)
 
-axes = [
-    dict(
-        tag="wght",
-        name="Weight",
-        values=[
-            dict(value=100, name='Thin'),
-            dict(value=200, name='Extralight'),
-            dict(value=300, name='Light'),
-            dict(value=400, name='Regular', flags=0x2, linkedValue=700),
-            dict(value=500, name='Medium'),
-            dict(value=600, name='Semibold'),
-            dict(value=700, name='Bold'),
-            dict(value=800, name='Extrabold'),
-            dict(value=900, name='Black'),
-        ],
-    ),
-    dict(
-        tag="RDNS",
-        name="Roundness",
-        values=[
-            dict(value=-1, name='Regular', flags=0x2),
-            dict(value=0, name='Softened'),
-            dict(value=1, name='Rounded'),
-        ],
-    )
+wghtAxisLabels = [
+    AxisLabelDescriptor(name="Thin", userValue=100),
+    AxisLabelDescriptor(name="ExtraLight", userValue=200),
+    AxisLabelDescriptor(name="Light", userValue=300),
+    AxisLabelDescriptor(name="Regular", userValue=400, elidable=True, linkedUserValue=700),
+    AxisLabelDescriptor(name="Medium", userValue=500),
+    AxisLabelDescriptor(name="SemiBold", userValue=600),
+    AxisLabelDescriptor(name="Bold", userValue=700),
+    AxisLabelDescriptor(name="ExtraBold", userValue=800),
+    AxisLabelDescriptor(name="Black", userValue=900),
+]
+dsfile.getAxisByTag("wght").axisLabels = wghtAxisLabels
+
+RDNSAxisLabels = [
+    AxisLabelDescriptor(name="Regular", userValue=-1, elidable=True),
+    AxisLabelDescriptor(name="Softened", userValue=0),
+    AxisLabelDescriptor(name="Rounded", userValue=1),
 ]
 
+dsfile.getAxisByTag("RDNS").axisLabels = RDNSAxisLabels
 
-buildStatTable(font, axes=axes)
-font.save(path)
+locationLabels = [
+    LocationLabelDescriptor(name="Thin", userLocation=dict(Roundness=-1, Weight=100)),
+    LocationLabelDescriptor(name="ExtraLight", userLocation=dict(Roundness=-1, Weight=200)),
+    LocationLabelDescriptor(name="Light", userLocation=dict(Roundness=-1, Weight=300)),
+    LocationLabelDescriptor(name="Regular", userLocation=dict(Roundness=-1, Weight=400)),
+    LocationLabelDescriptor(name="Medium", userLocation=dict(Roundness=-1, Weight=500)),
+    LocationLabelDescriptor(name="SemiBold", userLocation=dict(Roundness=-1, Weight=600)),
+    LocationLabelDescriptor(name="Bold", userLocation=dict(Roundness=-1, Weight=700)),
+    LocationLabelDescriptor(name="ExtraBold", userLocation=dict(Roundness=-1, Weight=800)),
+    LocationLabelDescriptor(name="Black", userLocation=dict(Roundness=-1, Weight=900)),
+    
+    LocationLabelDescriptor(name="Thin Softened", userLocation=dict(Roundness=0, Weight=100)),
+    LocationLabelDescriptor(name="ExtraLight Softened", userLocation=dict(Roundness=0, Weight=200)),
+    LocationLabelDescriptor(name="Light Softened", userLocation=dict(Roundness=0, Weight=300)),
+    LocationLabelDescriptor(name="Softened", userLocation=dict(Roundness=0, Weight=400)),
+    LocationLabelDescriptor(name="Medium Softened", userLocation=dict(Roundness=0, Weight=500)),
+    LocationLabelDescriptor(name="SemiBold Softened", userLocation=dict(Roundness=0, Weight=600)),
+    LocationLabelDescriptor(name="Bold Softened", userLocation=dict(Roundness=0, Weight=700)),
+    LocationLabelDescriptor(name="ExtraBold Softened", userLocation=dict(Roundness=0, Weight=800)),
+    LocationLabelDescriptor(name="Black Softened", userLocation=dict(Roundness=0, Weight=900)),
+    
+    LocationLabelDescriptor(name="Thin Rounded", userLocation=dict(Roundness=1, Weight=100)),
+    LocationLabelDescriptor(name="ExtraLight Rounded", userLocation=dict(Roundness=1, Weight=200)),
+    LocationLabelDescriptor(name="Light Rounded", userLocation=dict(Roundness=1, Weight=300)),
+    LocationLabelDescriptor(name="Rounded", userLocation=dict(Roundness=1, Weight=400)),
+    LocationLabelDescriptor(name="Medium Rounded", userLocation=dict(Roundness=1, Weight=500)),
+    LocationLabelDescriptor(name="SemiBold Rounded", userLocation=dict(Roundness=1, Weight=600)),
+    LocationLabelDescriptor(name="Bold Rounded", userLocation=dict(Roundness=1, Weight=700)),
+    LocationLabelDescriptor(name="ExtraBold Rounded", userLocation=dict(Roundness=1, Weight=800)),
+    LocationLabelDescriptor(name="Black Rounded", userLocation=dict(Roundness=1, Weight=900)),
+]
+for label in locationLabels:
+    dsfile.addLocationLabel(label)
+
+dsfile.write(path)
